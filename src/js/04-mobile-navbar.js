fe7e46c0db0af0352228ab5529790ef6d8b9a550
Arne Vandamme <arne@foreach.be>
2018-04-08 10:27:04 +0200

    Fix mobile styling

--- src/js/04-mobile-navbar.js
+++ src/js/04-mobile-navbar.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var navbarToggles = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  var navbarToggles = Array.prototype.slice.call(document.querySelectorAll('.js-overlay-menu-button'), 0)
   if (navbarToggles.length === 0) return
   navbarToggles.forEach(function (el) {
     el.addEventListener('click', function (e) {
       e.stopPropagation()
       el.classList.toggle('is-active')
-      document.getElementById(el.dataset.target).classList.toggle('is-active')
+      document.getElementById(el.dataset.target).classList.toggle('is-open')
       document.documentElement.classList.toggle('is-clipped--navbar')
     })
   })
